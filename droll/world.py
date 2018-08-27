c3f898c875793ff90feaf71332f39366fbbac942
Rhys Ulerich <rhys.ulerich@gmail.com>
2018-08-26 22:28:40 -0400

    new game/delve/level

--- droll/world.py
+++ droll/world.py
@@ -3,6 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import collections
+import copy
 import functools
 import itertools
 import typing
@@ -32,12 +33,12 @@ def _roll(
     return result
 
 
-def roll_level(dice: int, randgen: RandRange) -> Level:
+def roll_level(dice: int, randrange: RandRange) -> Level:
     """Roll a new Level using given number of dice.
 
     On Level N one should account for the number of extant dragons.
-    random.Random.randgen or random.randgen are accepted for randomness."""
-    return Level(*_roll(dice, 0, len(Level._fields), randgen))
+    random.Random.randrange or random.randrange are accepted for randomness."""
+    return Level(*_roll(dice, 0, len(Level._fields), randrange))
 
 
 Party = collections.namedtuple('Party', (
@@ -50,14 +51,14 @@ Party = collections.namedtuple('Party', (
 ))
 
 
-def roll_party(dice: int, randgen: RandRange) -> Party:
+def roll_party(dice: int, randrange: RandRange) -> Party:
     """Roll a new Party using given number of dice.
 
-    random.Random.randgen or random.randgen are accepted for randomness."""
-    return Party(*_roll(dice, 0, len(Party._fields), randgen))
+    random.Random.randrange or random.randrange are accepted for randomness."""
+    return Party(*_roll(dice, 0, len(Party._fields), randrange))
 
 
-_TREASURE = collections.OrderedDict((
+_CHEST = collections.OrderedDict((
     ('sword', 3),
     ('talisman', 3),
     ('sceptre', 3),
@@ -70,28 +71,28 @@ _TREASURE = collections.OrderedDict((
     ('scale', 6),
 ))
 
-Treasure = collections.namedtuple('Treasure', _TREASURE.keys())
+Treasure = collections.namedtuple('Treasure', _CHEST.keys())
 
-TREASURE_INITIAL = Treasure(*_TREASURE.values())
+CHEST_INITIAL = Treasure(*_CHEST.values())
 
-Choice = typing.Callable[[typing.Sequence[typing.Any]], typing.Any]
+TREASURE_INITIAL = Treasure(*([0] * len(_CHEST)))
 
-
-def _draw(chest: Treasure, choice: Choice) -> typing.Tuple[Treasure, Treasure]:
+def _draw(chest: Treasure, randrange: RandRange) -> str:
     seq = functools.reduce(
         itertools.chain,
         (itertools.repeat(t, chest[i])
          for i, t in enumerate(Treasure._fields)),
         [])
-    return choice(tuple(seq))
+    seq = tuple(seq)
+    return seq[randrange(0, len(seq))]
 
 
 def draw_treasure(
         treasure: Treasure,
         chest: Treasure,
-        choice: Choice,
+        randrange: RandRange,
 ) -> typing.Tuple[Treasure, Treasure]:
-    drawn = _draw(chest=chest, choice=choice)
+    drawn = _draw(chest=chest, randrange=randrange)
     treasure = treasure._replace(**{drawn: getattr(treasure, drawn) + 1})
     chest = chest._replace(**{drawn: getattr(chest, drawn) - 1})
     return treasure, chest
@@ -109,6 +110,7 @@ def replace_treasure(
 
 
 World = collections.namedtuple('World', (
+    'depth',
     'experience',
     'ability',
     'level',
@@ -116,3 +118,36 @@ World = collections.namedtuple('World', (
     'treasure',
     'chest',
 ))
+
+
+def new_game() -> World:
+    """Establish a new game independent of a delve/level."""
+    return World(
+        depth=None,
+        experience=0,
+        ability=None,
+        level=None,
+        party=None,
+        treasure=copy.deepcopy(TREASURE_INITIAL),
+        chest=copy.deepcopy(CHEST_INITIAL),
+    )
+
+
+def new_delve(world: World, randrange: RandRange) -> World:
+    """Establish a new delve within an existing game."""
+    return world._replace(
+        depth=0,
+        ability=True,
+        level=roll_level(dice=1, randrange=randrange),
+        party=roll_party(dice=7, randrange=randrange),
+    )
+
+
+def next_level(world: World, randrange: RandRange) -> World:
+    """Move one level deeper in the dungeon."""
+    next_depth = world.depth + 1
+    assert next_depth <= 10
+    prior_dragons = world.level.dragon
+    level = roll_level(dice=(next_depth - prior_dragons), randrange=randrange)
+    level = level._replace(dragon=level.dragon + prior_dragons)
+    return world._replace(depth=next_depth, level=level)
