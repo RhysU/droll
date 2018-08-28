75c39b6d1c8ab3fe578241d11a3e4527983e95ff
Rhys Ulerich <rhys.ulerich@gmail.com>
2018-08-27 22:05:00 -0400

    Stub out fighting

---
+++ droll/fight.py
@@ -0,0 +1,69 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import typing
+
+from droll.world import Level, Party
+
+# Encodes default hero-vs-enemy capabilities
+_MANY_DEFAULT = Party(
+    fighter=Level(
+        goblin=True,
+        skeleton=False,
+        ooze=False,
+        chest=False,
+        potion=True,
+        dragon=False,
+    ),
+    cleric=Level(
+        goblin=False,
+        skeleton=True,
+        ooze=False,
+        chest=False,
+        potion=True,
+        dragon=False,
+    ),
+    mage=Level(
+        goblin=False,
+        skeleton=False,
+        ooze=True,
+        chest=False,
+        potion=True,
+        dragon=False,
+    ),
+    thief=Level(
+        goblin=False,
+        skeleton=False,
+        ooze=False,
+        chest=True,
+        potion=True,
+        dragon=False,
+    ),
+    champion=Level(
+        goblin=True,
+        skeleton=True,
+        ooze=True,
+        chest=True,
+        potion=True,
+        dragon=False,
+    ),
+    scroll=Level(
+        goblin=False,
+        skeleton=False,
+        ooze=False,
+        chest=False,
+        potion=False,
+        dragon=False,
+    ),
+)
+
+def survivors(level: Level, defender: str, attacker: str, *, prowess = _MANY_DEFAULT):
+    assert defender != 'dragon'  # Special case
+    assert attacker != 'scroll'  # Special case
+    prior = getattr(level, defender)
+    assert prior >= 1
+    if getattr(getattr(prowess, attacker), defender):
+        return level._replace(**{defender: 0})
+    else:
+        return level._replace(**{defender: prior -1})
---
+++ tests/test_fight.py
@@ -0,0 +1,27 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import pytest
+import random
+
+import droll.fight
+
+@pytest.fixture(name='party')
+def _party():
+    return droll.fight.Party(*([1] * len(droll.fight.Party._fields)))
+
+def test_fighter(party):
+    pass
+
+def test_cleric(party):
+    pass
+
+def test_mage(party):
+    pass
+
+def test_thief(party):
+    pass
+
+def test_champion(party):
+    pass
