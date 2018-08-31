06ebac40de7c2b3090f054084572d4ca928871bc
Rhys Ulerich <rhys.ulerich@gmail.com>
2018-08-30 23:11:48 -0400

    Draft of chests

--- droll/player.py
+++ droll/player.py
@@ -34,17 +34,49 @@ def _defeat_some(
 def _defeat_one(world: World, hero: str, *defenders: typing.List[str]) -> World:
     """Update world after regular hero defeats exactly one defender."""
     return _defeat_some(remaining=lambda prior_defenders: prior_defenders - 1,
-                        world=world,
-                        hero=hero,
-                        *defenders)
+                        world=world, hero=hero, *defenders)
 
 
 def _defeat_all(world: World, hero: str, *defenders: typing.List[str]) -> World:
     """Update world after regular hero defeats all of one type of defender."""
     return _defeat_some(remaining=lambda _: 0,
-                        world=world,
-                        hero=hero,
-                        *defenders)
+                        world=world, hero=hero, *defenders)
+
+
+# Reduces boilerplate in _open_one and _open_all
+def _open_some(
+        remaining: typing.Callable[[int], int],
+        world: World,
+        hero: str,
+        *defenders: typing.List[str]
+) -> World:
+    """Update world after regular hero open exactly some chests."""
+    prior_heroes = getattr(world.party, hero)
+    assert prior_heroes >= 1, "Require at least one {}".format(hero)
+    defender, *_ = defenders
+    assert defender == 'chest', "Special logic does not handle {}".format(defender)
+    prior_defenders = getattr(world.level, defender)
+    assert prior_defenders >= 1, "Expected at least one {}".format(defender)
+    remaining_defenders = remaining(prior_defenders)
+    world = world._replace(
+            party=world.party._replace(**{hero: prior_heroes - 1}),
+            level=world.level._replace(**{defender: remaining_defenders}),
+    )
+    for _ in range(prior_defenders - remaining_defenders):
+        world = droll.world.draw_treasure(world, randrange)
+    return world
+
+
+def _open_one(world: World, hero: str, *defenders: typing.List[str]) -> World:
+    """Update world after regular hero opens exactly one chest."""
+    return _open_some(remaining=lambda prior_chests: prior_chests - 1,
+                      world=world, hero=hero, *chests)
+
+
+def _open_all(world: World, hero: str, *chests: typing.List[str]) -> World:
+    """Update world after regular hero opens all chests."""
+    return _open_some(remaining=lambda _: 0,
+                      world=world, hero=hero, *chests)
 
 
 # Encodes default hero-vs-enemy capabilities
