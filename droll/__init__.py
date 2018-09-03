a88d14350117e2947d9304fcee2788bd95258053
Rhys Ulerich <rhys.ulerich@gmail.com>
2018-09-03 12:26:21 -0400

    Draft of top-level class

--- droll/__init__.py
+++ droll/__init__.py
@@ -2,7 +2,50 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """Top-level entry points and basic utilities."""
+import random
 import typing
+
+import droll.player
+import droll.world
+
+
+class Droll:
+    """Top-level entry point for playing a game of Droll."""
+
+    def __init__(self, player=droll.player.DEFAULT, randrange=None):
+        self._player = player
+        self._randrange = (random.Random().randrange
+                           if randrange is None else randrange)
+        self._world = droll.world.new_game()
+        self._world = droll.world.new_delve(self._world, self._randrange)
+        self._world = droll.world.next_level(self._world, self._randrange)
+
+    def score(self) -> int:
+        return droll.world.score(self._world)
+
+    def hero(self, *nouns) -> 'Droll':
+        self._world = droll.player.apply(
+            self._player, self._world, self._randrange, *nouns)
+        return self
+
+    def retire(self) -> 'Droll':
+        self._world = droll.world.retire(self._world)
+        return self
+
+    def retreat(self) -> 'Droll':
+        self._world = droll.world.new_delve(self._world, self._randrange)
+        return self
+
+    def treasure(self, *nouns) -> 'Droll':
+        raise NotImplementedError("FIXME")
+        return self
+
+    def ability(self, *nouns) -> 'Droll':
+        raise NotImplementedError("FIXME")
+        return self
+
+    def __str__(self):
+        return brief(self)
 
 
 def brief(o: typing.Any) -> str:
