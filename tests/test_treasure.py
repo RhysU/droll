9eb443e9c3895f510f31f6aae167b7bc221ff938
Rhys Ulerich <rhys.ulerich@gmail.com>
2018-09-03 21:02:44 -0400

    Add test_treasure for elixir

---
+++ tests/test_treasure.py
@@ -0,0 +1,34 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""Testing of world-to-world transitions stemming from using treasure."""
+
+import random
+
+import pytest
+
+import droll.player as player
+import droll.world as world
+
+
+@pytest.fixture(name='game')
+def _game():
+    return world.new_game()._replace(
+        level=world.Level(*([2] * len(world.Level._fields))),
+        party=world.Party(*([2] * len(world.Party._fields))),
+    )
+
+
+@pytest.fixture(name='randrange')
+def _randrange():
+    return random.Random(4).randrange
+
+
+def test_elixir(game, randrange):
+    game = game._replace(treasure=game.treasure._replace(elixir=1))
+    game = player.apply(player.DEFAULT, game, randrange, 'elixir', 'cleric')
+    assert game.party.cleric == 3
+    assert game.treasure.elixir == 0
+
+    with pytest.raises(world.WorldError):
+        player.apply(player.DEFAULT, game, randrange, 'elixir', 'mage')
