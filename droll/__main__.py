b2c5ada064e38eda29dc55b2ead0d121fb33cf1a
Rhys Ulerich <rhys.ulerich@gmail.com>
2018-09-08 13:13:56 -0400

    Permit setting the seed

--- droll/__main__.py
+++ droll/__main__.py
@@ -2,11 +2,16 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """Command-line version of droll."""
+import argparse
+import random
 
 from .shell import Shell
 
-# TODO Permit specifying seeds
 # TODO Permit specifying different players
 if __name__ == '__main__':
-    s = Shell()
+    parser = argparse.ArgumentParser(prog='droll', description=__doc__)
+    parser.add_argument('--seed', metavar='N', type=int, nargs=1, default=None,
+                        help='an integer to seed random number generation')
+    arguments = parser.parse_args()
+    s = Shell(randrange=random.Random(*arguments.seed).randrange)
     s.cmdloop()
