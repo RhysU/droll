4cedbf59c689bb5aba0cf47ee24026f794a4e664
Rhys Ulerich <rhys.ulerich@gmail.com>
2026-02-18 23:00:19 -0500

    More deliberate imports

--- droll/dungeon.py
+++ droll/dungeon.py
@@ -6,7 +6,7 @@
 from dataclasses import replace
 
 from .error import DrollError
-from . import struct
+from .struct import Dungeon, field_values
 
 __all__ = (
     "blocking_dragon",
@@ -19,32 +19,32 @@ __all__ = (
 )
 
 
-def defeated_monsters(dungeon: struct.Dungeon) -> bool:
+def defeated_monsters(dungeon: Dungeon) -> bool:
     """Are all non-dragon monsters on this dungeon defeated?"""
     return (dungeon is None) or (dungeon.goblin + dungeon.skeleton + dungeon.ooze) == 0
 
 
-def defeated_dungeon(dungeon: struct.Dungeon) -> bool:
+def defeated_dungeon(dungeon: Dungeon) -> bool:
     """Are all monsters and any dragon on this dungeon defeated?"""
     return (dungeon is None) or (defeated_monsters(dungeon) and dungeon.dragon < 3)
 
 
-def blocking_dragon(dungeon: struct.Dungeon) -> bool:
+def blocking_dragon(dungeon: Dungeon) -> bool:
     """Is a dragon blocking progress to the next level?"""
     return defeated_monsters(dungeon) and not defeated_dungeon(dungeon)
 
 
-def exhausted_dungeon(dungeon: struct.Dungeon) -> bool:
+def exhausted_dungeon(dungeon: Dungeon) -> bool:
     """Has the player exhausted all possible actions for this dungeon?
 
     In contrast to defeated_dungeon(...), returns True if chests/etc remain."""
     return (dungeon is None) or (
-        (sum(struct.field_values(dungeon)) - dungeon.dragon == 0)
+        (sum(field_values(dungeon)) - dungeon.dragon == 0)
         and not blocking_dragon(dungeon)
     )
 
 
-def decrement_dungeon(dungeon: struct.Dungeon, target: str) -> struct.Dungeon:
+def decrement_dungeon(dungeon: Dungeon, target: str) -> Dungeon:
     """Decrease the count of the specified target type by one."""
     if dungeon is None:
         raise DrollError("No dungeon currently active.")
@@ -54,7 +54,7 @@ def decrement_dungeon(dungeon: struct.Dungeon, target: str) -> struct.Dungeon:
     return replace(dungeon, **{target: prior_targets - 1})
 
 
-def increment_dungeon(dungeon: struct.Dungeon, target: str) -> struct.Dungeon:
+def increment_dungeon(dungeon: Dungeon, target: str) -> Dungeon:
     """Increase the count of the specified target type by one."""
     if dungeon is None:
         raise DrollError("No dungeon currently active.")
@@ -62,7 +62,7 @@ def increment_dungeon(dungeon: struct.Dungeon, target: str) -> struct.Dungeon:
     return replace(dungeon, **{target: prior_targets + 1})
 
 
-def eliminate_dungeon(dungeon: struct.Dungeon, target: str) -> struct.Dungeon:
+def eliminate_dungeon(dungeon: Dungeon, target: str) -> Dungeon:
     """Remove all targets of the specified type from the dungeon."""
     if dungeon is None:
         raise DrollError("No dungeon currently active.")
